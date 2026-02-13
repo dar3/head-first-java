d2eb1f14f4a9b725462d5bc9c4c50a72f9e15304
Darko <darkoshypka@gmail.com>
2026-02-13 22:37:12 +0100

    declaring without initializing variables test

---
+++ src/chapter04/PoorDogTestDrive.java
@@ -0,0 +1,11 @@
+package chapter04;
+
+public class PoorDogTestDrive {
+
+    public static void main(String[] args) {
+
+        PoorDog dog1 = new PoorDog();
+        System.out.println("Dog size is: " + dog1.getSize());
+        System.out.println("Dog name is: " + dog1.getName());
+    }
+}
