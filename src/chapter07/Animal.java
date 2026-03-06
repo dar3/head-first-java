90442c2c834295ec6d03b39be06ac9ece3b528b2
Darko <darkoshypka@gmail.com>
2026-03-06 23:58:29 +0100

    feat: start implementing inheritance among Animals

---
+++ src/chapter07/Animal.java
@@ -0,0 +1,15 @@
+package chapter07;
+
+public class Animal {
+
+    String picture;
+    String food;
+    int hunger;
+    int boundaries;
+    String location;
+
+    void makeNoise(){}
+    void eat(){}
+    void sleep(){}
+    void roam(){}
+}
