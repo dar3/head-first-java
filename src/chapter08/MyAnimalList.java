49c016c918ddfa20817c9153218c5377d32052bb
Darko <darkoshypka@gmail.com>
2026-03-09 22:38:03 +0100

    add: Animal array from scratch

---
+++ src/chapter08/MyAnimalList.java
@@ -0,0 +1,17 @@
+package chapter08;
+
+import chapter07.Animal;
+
+public class MyAnimalList {
+
+        private Animal[] animals = new Animal[5];
+        private int nextIndex = 0;
+        public void add(Animal a) {
+            if (nextIndex < animals.length) {
+                animals[nextIndex] = a;
+                System.out.println("Animal added at " + nextIndex);
+                nextIndex++;
+            }
+        }
+    }
+
