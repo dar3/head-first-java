526730b2181c60dffcd8db6ee56fac4b5a29f3d9
Darko <darkoshypka@gmail.com>
2026-03-09 22:33:36 +0100

    feat: create array from scratch

---
+++ src/chapter08/MyDogList.java
@@ -0,0 +1,17 @@
+package chapter08;
+
+import chapter07.Dog;
+
+public class MyDogList {
+
+    private Dog[] dogs = new Dog[5];
+    private int nextIndex = 0;
+
+    public void add(Dog d) {
+        if (nextIndex < dogs.length) {
+            dogs[nextIndex] = d;
+            System.out.println("Dog added at " + nextIndex);
+            nextIndex++;
+        }
+    }
+}
